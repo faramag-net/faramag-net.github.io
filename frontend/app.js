9670a361a13fb8e0eabb3ef098be164274b34f5e
faramag-net <faratool@gmail.com>
2026-05-21 09:25:27 -0600

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,48 @@
+const form = document.getElementById('productForm');
+const tabla = document.getElementById('tablaProductos');
 
+let productos = [];
+
+form.addEventListener('submit', function(e) {
+
+  e.preventDefault();
+
+  const producto = document.getElementById('producto').value;
+  const precio = document.getElementById('precio').value;
+  const tienda = document.getElementById('tienda').value;
+  const contacto = document.getElementById('contacto').value;
+
+  const fecha = new Date().toLocaleString();
+
+  const nuevoProducto = {
+    producto,
+    precio,
+    tienda,
+    contacto,
+    fecha
+  };
+
+  productos.push(nuevoProducto);
+
+  mostrarProductos();
+
+  form.reset();
+});
+
+function mostrarProductos() {
+
+  tabla.innerHTML = '';
+
+  productos.forEach(p => {
+
+    tabla.innerHTML += `
+      <tr>
+        <td>${p.producto}</td>
+        <td>$${p.precio}</td>
+        <td>${p.tienda}</td>
+        <td>${p.contacto}</td>
+        <td>${p.fecha}</td>
+      </tr>
+    `;
+  });
+}
