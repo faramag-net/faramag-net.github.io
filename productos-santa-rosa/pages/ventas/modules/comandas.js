0f630ef7b44248c697faba60813c8de0ed4c84d4
faramag-net <faratool@gmail.com>
2026-05-24 11:05:32 -0600

    Update comandas.js

--- productos-santa-rosa/pages/ventas/modules/comandas.js
+++ productos-santa-rosa/pages/ventas/modules/comandas.js
@@ -1,1 +1,90 @@
+import {
+    ventas,
+    comanda,
+    guardarVentas
+}
+from "./storage.js";
 
+import {
+    obtenerProductos
+}
+from "./inventario-api.js";
+
+import {
+    actualizarKPIs,
+    renderTablaVentas
+}
+from "./historial.js";
+
+export function agregarComanda(){
+
+    const productos =
+    obtenerProductos();
+
+    const nombre =
+    document.getElementById("producto").value;
+
+    const cliente =
+    document.getElementById("cliente").value;
+
+    const cantidad =
+    Number(
+        document.getElementById("cantidad").value
+    );
+
+    const producto =
+    productos.find(
+        p => p.nombre === nombre
+    );
+
+    if(!producto){
+
+        alert("Producto no encontrado");
+
+        return;
+
+    }
+
+    if(cantidad <= 0){
+
+        alert("Cantidad inválida");
+
+        return;
+
+    }
+
+    if(cantidad > producto.stock){
+
+        alert("Inventario insuficiente");
+
+        return;
+
+    }
+
+    const subtotal =
+    cantidad * producto.precio;
+
+    const ganancia =
+    subtotal - (cantidad * producto.costo);
+
+    comanda.push({
+
+        producto: nombre,
+
+        cliente,
+
+        cantidad,
+
+        precio: producto.precio,
+
+        costo: producto.costo,
+
+        subtotal,
+
+        ganancia
+
+    });
+
+    renderComanda();
+
+}
