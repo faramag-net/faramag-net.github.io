cd3791cf0279ba2625fab945ff4c2c82df0d6b9d
faramag-net <faratool@gmail.com>
2026-05-24 12:36:34 -0600

    Update navbar.js

--- productos-santa-rosa/components/navbar.js
+++ productos-santa-rosa/components/navbar.js
@@ -1,1 +1,51 @@
+export function crearNavbar(){
 
+    return `
+
+    <nav class="navbar">
+
+        <div class="logo-navbar">
+
+            🍦 Santa Rosa
+
+        </div>
+
+        <div class="links-navbar">
+
+            <a href="/productos-santa-rosa/index.html">
+
+                🏠 Inicio
+
+            </a>
+
+            <a href="/productos-santa-rosa/pages/productos/index.html">
+
+                🍦 Productos
+
+            </a>
+
+            <a href="/productos-santa-rosa/pages/inventario/index.html">
+
+                📦 Inventario
+
+            </a>
+
+            <a href="/productos-santa-rosa/pages/ventas/index.html">
+
+                💰 Ventas
+
+            </a>
+
+            <a href="/productos-santa-rosa/pages/visitas/index.html">
+
+                📍 Visitas
+
+            </a>
+
+        </div>
+
+    </nav>
+
+    `;
+
+}
