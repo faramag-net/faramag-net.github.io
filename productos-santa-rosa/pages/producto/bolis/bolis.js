e750a4b41ff2a5407d1b459216d4e9c6ca6eb4f5
faramag-net <faratool@gmail.com>
2026-05-23 21:33:35 -0600

    Update bolis.js

--- productos-santa-rosa/pages/producto/bolis/bolis.js
+++ productos-santa-rosa/pages/producto/bolis/bolis.js
@@ -1,1 +1,54 @@
+const productos = [
+  {
+    nombre: "Mango",
+    precio: 20,
+    imagen: "../../../../imagenes/bcafe.jpg",
+    mensaje: "Quiero boli de mango"
+  },
 
+  {
+    nombre: "Fresa",
+    precio: 20,
+    imagen: "../../../../imagenes/bfresa.jpg",
+    mensaje: "Quiero boli de fresa"
+  },
+
+  {
+    nombre: "Chocolate",
+    precio: 20,
+    imagen: "../../../../imagenes/bchocolate.jpg",
+    mensaje: "Quiero boli de chocolate"
+  },
+
+  {
+    nombre: "Limón",
+    precio: 20,
+    imagen: "../../../../imagenes/blimon.jpg",
+    mensaje: "Quiero boli de limón"
+  }
+];
+
+const grid = document.getElementById("grid");
+
+productos.forEach(producto => {
+
+  const card = document.createElement("div");
+
+  card.classList.add("producto");
+
+  card.innerHTML = `
+    <img src="${producto.imagen}" alt="${producto.nombre}">
+
+    <p>${producto.nombre} - $${producto.precio}</p>
+
+    <a class="btn"
+       href="https://wa.me/5212225655003?text=${encodeURIComponent(producto.mensaje)}"
+       target="_blank">
+
+       Pedir por WhatsApp
+    </a>
+  `;
+
+  grid.appendChild(card);
+
+});
