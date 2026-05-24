a1a47666ed7269c3ee8f3a83a6c05bd004d2ebfc
faramag-net <faratool@gmail.com>
2026-05-24 10:38:49 -0600

    Update entradas.js

--- productos-santa-rosa/pages/inventario/modules/entradas.js
+++ productos-santa-rosa/pages/inventario/modules/entradas.js
@@ -1,1 +1,75 @@
+import {
+    productos,
+    movimientos,
+    guardarLocal
+}
+from "./storage.js";
 
+export function agregarInventario(){
+
+    const nombre =
+    document.getElementById("productoInventario").value;
+
+    const cantidad =
+    Number(
+        document.getElementById("agregarStock").value
+    );
+
+    const tipo =
+    document.getElementById("tipoMovimiento").value;
+
+    const producto =
+    productos.find(
+        p => p.nombre === nombre
+    );
+
+    if(!producto){
+
+        alert("Producto no encontrado");
+
+        return;
+
+    }
+
+    if(cantidad <= 0){
+
+        alert("Cantidad inválida");
+
+        return;
+
+    }
+
+    if(
+        tipo === "MERMA" ||
+        tipo === "CORTESIA"
+    ){
+
+        producto.stock -= cantidad;
+
+    }else{
+
+        producto.stock += cantidad;
+
+    }
+
+    movimientos.push({
+
+        tipo,
+
+        producto: nombre,
+
+        cantidad,
+
+        stock: producto.stock,
+
+        fecha: new Date().toLocaleString()
+
+    });
+
+    guardarLocal();
+
+    document.getElementById("agregarStock").value = "";
+
+    alert("Movimiento registrado");
+
+}
