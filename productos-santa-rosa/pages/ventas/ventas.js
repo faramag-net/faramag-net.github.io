09afb99b8dcb5c465fa91b743201ba517809c6eb
faramag-net <faratool@gmail.com>
2026-05-24 10:59:19 -0600

    Update ventas.js

--- productos-santa-rosa/pages/ventas/ventas.js
+++ productos-santa-rosa/pages/ventas/ventas.js
@@ -1,1 +1,21 @@
+import {
+    cargarProductos,
+    actualizarProducto,
+    actualizarSubtotal,
+    cambiarCantidad,
+    registrarVenta
+}
+from "./modules/registrar-venta.js";
 
+window.actualizarProducto =
+actualizarProducto;
+
+window.cambiarCantidad =
+cambiarCantidad;
+
+window.registrarVenta =
+registrarVenta;
+
+cargarProductos();
+
+actualizarSubtotal();
