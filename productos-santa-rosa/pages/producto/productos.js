c5bf740845b069e8aea588861138e659820270ed
faramag-net <faratool@gmail.com>
2026-05-24 09:56:22 -0600

    Update productos.js

--- productos-santa-rosa/pages/producto/productos.js
+++ productos-santa-rosa/pages/producto/productos.js
@@ -1,1 +1,36 @@
-productos.js
+const categorias = [
+
+  {
+    nombre: "Bolis",
+    emoji: "🧊",
+    clase: "bolis",
+    enlace: "./bolis/index.html"
+  },
+
+  {
+    nombre: "Paletas",
+    emoji: "🍦",
+    clase: "paletas",
+    enlace: "./paletas/index.html"
+  }
+
+];
+
+const contenedor = document.getElementById("categorias");
+
+categorias.forEach(categoria => {
+
+  const boton = document.createElement("a");
+
+  boton.classList.add("btn");
+  boton.classList.add(categoria.clase);
+
+  boton.href = categoria.enlace;
+
+  boton.innerHTML = `
+    ${categoria.emoji} Ver ${categoria.nombre}
+  `;
+
+  contenedor.appendChild(boton);
+
+});
