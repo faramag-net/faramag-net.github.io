e30fad3a0cc5999f6c69cbecbdb18c0a1fda5ca5
faramag-net <faratool@gmail.com>
2026-05-24 10:56:10 -0600

    Update storage.js

--- productos-santa-rosa/pages/ventas/modules/storage.js
+++ productos-santa-rosa/pages/ventas/modules/storage.js
@@ -1,1 +1,40 @@
+export let ventas = [];
 
+export let comanda = [];
+
+export function guardarVentas(){
+
+    localStorage.setItem(
+
+        "ventasSantaRosa",
+
+        JSON.stringify({
+
+            ventas,
+            comanda
+
+        })
+
+    );
+
+}
+
+export function cargarVentas(){
+
+    const datos =
+    localStorage.getItem(
+        "ventasSantaRosa"
+    );
+
+    if(!datos) return;
+
+    const sistema =
+    JSON.parse(datos);
+
+    ventas =
+    sistema.ventas || [];
+
+    comanda =
+    sistema.comanda || [];
+
+}
