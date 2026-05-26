fc84d69bb9f3808e5c10ad241ce739afaaffef65
faramag-net <faratool@gmail.com>
2026-05-25 19:12:18 -0600

    Update comandas.js

--- productos-santa-rosa/pages/ventas/modules/comandas.js
+++ productos-santa-rosa/pages/ventas/modules/comandas.js
@@ -5,10 +5,20 @@ import {
 }
 from "./storage.js";
 
-import {
-    obtenerProductos
+function obtenerProductos(){
+
+    const inventario =
+    JSON.parse(
+
+        localStorage.getItem(
+            "inventarioSantaRosa"
+        )
+
+    ) || {};
+
+    return inventario.productos || [];
+
 }
-from "./inventario-api.js";
 
 import {
     actualizarKPIs,
