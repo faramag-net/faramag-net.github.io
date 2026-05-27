39198ed7245326501fb7139ef688ae9e8acde7c3
faramag-net <faratool@gmail.com>
2026-05-27 09:40:26 -0600

    Update storage.js

--- productos-santa-rosa/pages/ventas/modules/storage.js
+++ productos-santa-rosa/pages/ventas/modules/storage.js
@@ -2,20 +2,14 @@ import LocalDB from "../../../core/storage/local-db.js";
 
 export let productos = [];
 
-export let movimientos = [];
-
 export function guardarLocal() {
 
     LocalDB.saveProducts(productos);
 
-    LocalDB.saveMovements(movimientos);
 }
 
 export function cargarLocal() {
 
     productos =
         LocalDB.getProducts();
-
-    movimientos =
-        LocalDB.getMovements();
 }
