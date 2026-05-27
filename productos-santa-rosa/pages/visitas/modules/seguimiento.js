09f84522b1996dc274db729efc16477d09cc43ae
faramag-net <faratool@gmail.com>
2026-05-27 13:48:38 -0600

    Update seguimiento.js

--- productos-santa-rosa/pages/visitas/modules/seguimiento.js
+++ productos-santa-rosa/pages/visitas/modules/seguimiento.js
@@ -1,1 +1,34 @@
+import LocalDB from "../../../core/storage/local-db.js";
 
+export function actualizarSeguimiento(
+    clienteId,
+    updates
+){
+
+    const clientes =
+        LocalDB.getClients();
+
+    const updated =
+        clientes.map(cliente => {
+
+            if(cliente.id === clienteId){
+
+                return {
+
+                    ...cliente,
+
+                    ...updates,
+
+                    updatedAt:
+                        new Date().toISOString()
+
+                };
+            }
+
+            return cliente;
+        });
+
+    LocalDB.saveClients(updated);
+
+    return true;
+}
