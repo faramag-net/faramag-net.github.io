18c66ac6bff535b3e5924869ea69f851fa0bf18f
faramag-net <faratool@gmail.com>
2026-05-27 13:24:27 -0600

    Update render.js

--- productos-santa-rosa/pages/visitas/modules/render.js
+++ productos-santa-rosa/pages/visitas/modules/render.js
@@ -1,1 +1,58 @@
+import LocalDB from "../../../core/storage/local-db.js";
 
+export function renderClientes() {
+
+    const container =
+        document.getElementById(
+            "listaClientes"
+        );
+
+    if(!container) return;
+
+    const clientes =
+        LocalDB.getClients();
+
+    container.innerHTML = "";
+
+    clientes.forEach(cliente => {
+
+        let estado = "✅ OK";
+
+        let color = "#28a745";
+
+        if(cliente.saldo > 0){
+
+            estado = "💰 PENDIENTE";
+
+            color = "#dc3545";
+        }
+
+        container.innerHTML += `
+
+        <div class="card-cliente">
+
+            <h3>
+                ${cliente.nombre}
+            </h3>
+
+            <p>
+                📞 ${cliente.telefono || "-"}
+            </p>
+
+            <p>
+                📍 ${cliente.direccion || "-"}
+            </p>
+
+            <p style="color:${color}">
+                Saldo: $${cliente.saldo || 0}
+            </p>
+
+            <small>
+                ${estado}
+            </small>
+
+        </div>
+
+        `;
+    });
+}
