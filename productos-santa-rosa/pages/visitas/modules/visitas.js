47b48afa95d2900b7a85cb90889d567ae1a14db1
faramag-net <faratool@gmail.com>
2026-05-27 13:41:50 -0600

    Update visitas.js

--- productos-santa-rosa/pages/visitas/modules/visitas.js
+++ productos-santa-rosa/pages/visitas/modules/visitas.js
@@ -1,1 +1,32 @@
+import LocalDB from "../../../core/storage/local-db.js";
 
+export function registrarVisita(clienteId) {
+
+    const nota =
+        document.getElementById(
+            "notaVisita"
+        ).value;
+
+    const pedido =
+        document.getElementById(
+            "pedidoVisita"
+        ).value;
+
+    LocalDB.addVisit({
+
+        clienteId,
+
+        nota,
+
+        pedido,
+
+        status: "pending",
+
+        fecha:
+            new Date().toISOString()
+
+    });
+
+    alert("Visita registrada");
+
+}
