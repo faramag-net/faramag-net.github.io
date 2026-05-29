48f41071a68d225fbfb0c483bc29fd5a98cf524c
faramag-net <faratool@gmail.com>
2026-05-29 15:02:35 -0600

    Update historial.js

--- productos-santa-rosa/pages/ventas/modules/historial.js
+++ productos-santa-rosa/pages/ventas/modules/historial.js
@@ -1,7 +1,4 @@
-import {
-    ventas
-}
-from "./storage.js";
+import LocalDB from "../../../core/storage/local-db.js";
 
 export function renderTablaVentas(){
 
@@ -10,28 +7,23 @@ export function renderTablaVentas(){
 
     if(!tabla) return;
 
-    tabla.innerHTML = "";
+tabla.innerHTML = "";
 
-    ventas.forEach((venta,index)=>{
+const ventas = LocalDB.getSales();
+
+ventas.forEach((venta,index)=>{
 
         tabla.innerHTML += `
 
         <tr>
 
             <td>${venta.producto}</td>
-
             <td>${venta.cliente}</td>
-
             <td>${venta.cantidad}</td>
-
-            <td>$${venta.precio.toFixed(2)}</td>
-
-            <td>$${venta.costo.toFixed(2)}</td>
-
-            <td>$${venta.subtotal.toFixed(2)}</td>
-
-            <td>$${venta.ganancia.toFixed(2)}</td>
-
+            <td>$${Number(venta.precio || 0).toFixed(2)}</td>
+            <td>$${Number(venta.costo || 0).toFixed(2)}</td>
+            <td>$${Number(venta.total || 0).toFixed(2)}</td>
+            <td>$${Number(venta.ganancia || 0).toFixed(2)}</td>
             <td>${venta.fecha}</td>
 
         </tr>
@@ -44,19 +36,21 @@ export function renderTablaVentas(){
 
 export function actualizarKPIs(){
 
+    const ventas = LocalDB.getSales();
+    
     let totalVentas = 0;
 
     let totalGanancia = 0;
 
-    ventas.forEach(venta=>{
+ventas.forEach(venta=>{
 
-        totalVentas +=
-        Number(venta.subtotal) || 0;
+    totalVentas +=
+    Number(venta.total || 0);
 
-        totalGanancia +=
-        Number(venta.ganancia) || 0;
+    totalGanancia +=
+    Number(venta.ganancia || 0);
 
-    });
+});
 
     const ventasCard =
     document.getElementById("totalVentas");
