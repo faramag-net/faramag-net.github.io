6dd107ad4491ba6e3de9d89974eda77882bfa0c8
faramag-net <faratool@gmail.com>
2026-05-30 10:57:18 -0600

    Update dashboard.js

--- productos-santa-rosa/dashboard.js
+++ productos-santa-rosa/dashboard.js
@@ -15,3 +15,55 @@ console.log("Productos", productos);
 console.log("Inventario", inventario);
 
 console.log("Ventas", ventas);
+
+const totalProductos =
+    productos.length;
+
+const stockBajo =
+inventario.filter(
+    item =>
+        item.stock > 0 &&
+        item.stock <= 5
+);
+
+const totalVentas =
+ventas.reduce(
+
+    (acc, venta) =>
+
+    acc + Number(venta.total || 0),
+
+0
+
+);
+
+const totalGanancia =
+ventas.reduce(
+
+    (acc, venta) =>
+
+    acc + Number(venta.ganancia || 0),
+
+0
+
+);
+
+document
+.getElementById("kpiProductos")
+.innerText =
+totalProductos;
+
+document
+.getElementById("kpiStock")
+.innerText =
+stockBajo.length;
+
+document
+.getElementById("kpiVentas")
+.innerText =
+`$${totalVentas}`;
+
+document
+.getElementById("kpiGanancia")
+.innerText =
+`$${totalGanancia}`;
