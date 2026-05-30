01dd567ce6b0108553de2b3ecb342510f7e3d597
faramag-net <faratool@gmail.com>
2026-05-30 10:52:49 -0600

    Update dashboard.js

--- productos-santa-rosa/dashboard.js
+++ productos-santa-rosa/dashboard.js
@@ -1,1 +1,17 @@
+import LocalDB
+from "./core/storage/local-db.js";
 
+const productos =
+    LocalDB.getProducts();
+
+const inventario =
+    LocalDB.getInventory();
+
+const ventas =
+    LocalDB.getSales();
+
+console.log("Productos", productos);
+
+console.log("Inventario", inventario);
+
+console.log("Ventas", ventas);
