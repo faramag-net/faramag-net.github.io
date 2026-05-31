da119b82f14fcfe2ef4b4434e48f341c9ac7f5ed
faramag-net <faratool@gmail.com>
2026-05-30 19:37:10 -0600

    Update clientes.js

--- productos-santa-rosa/pages/mercado/modules/clientes.js
+++ productos-santa-rosa/pages/mercado/modules/clientes.js
@@ -1,1 +1,40 @@
+import LocalDB from "../../../core/storage/local-db.js";
 
+export function getClientes() {
+    return LocalDB.getClients();
+}
+
+export function crearCliente(data) {
+
+    const clientes = LocalDB.getClients();
+
+    const nuevoCliente = {
+        id: crypto.randomUUID(),
+
+        nombre: data.nombre,
+
+        encargado: data.encargado || "",
+
+        telefono: data.telefono || "",
+
+        direccion: data.direccion || "",
+
+        latitud: "",
+
+        longitud: "",
+
+        comentarios: "",
+
+        estatus: "activo",
+
+        createdAt: new Date().toISOString(),
+
+        updatedAt: new Date().toISOString()
+    };
+
+    clientes.push(nuevoCliente);
+
+    LocalDB.saveClients(clientes);
+
+    return nuevoCliente;
+}
