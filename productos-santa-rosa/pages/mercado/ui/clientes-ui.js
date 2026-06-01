76db3155029b1b29cfc8152976a0b7f8ffe605e6
faramag-net <faratool@gmail.com>
2026-05-31 18:14:28 -0600

    Update clientes-ui.js

--- productos-santa-rosa/pages/mercado/ui/clientes-ui.js
+++ productos-santa-rosa/pages/mercado/ui/clientes-ui.js
@@ -1,1 +1,391 @@
+import {
+    
+    getClientes,
+    getClienteById,
+    crearCliente,
+    editarCliente,
+    eliminarCliente
+    
+} from "./modules/clientes.js";
 
+import {
+    setAbrirCliente
+}
+from "./shared-ui.js";
+
+setAbrirCliente(
+    abrirCliente
+);
+
+import {
+    nuevoProducto,
+    editarProductoUI,
+    eliminarProductoUI,
+    mostrarResumenProducto
+}
+from "./productos-ui.js";
+
+export function inicializarClientesUI(){
+    
+    document
+        .getElementById(
+            "btnNuevoCliente"
+        )
+        .addEventListener(
+            "click",
+            nuevoCliente
+        );
+
+    document
+        .getElementById(
+            "buscarCliente"
+        )
+        .addEventListener(
+            "input",
+            e =>
+                renderClientes(
+                    e.target.value
+                )
+        );
+
+    renderClientes();
+}
+
+function renderClientes(filtro = "") {
+
+    const lista =
+        document.getElementById(
+            "listaClientes"
+        );
+
+const clientes =
+    getClientes()
+        .filter(cliente =>
+            `
+                ${cliente.nombre || ""}
+                ${cliente.encargado || ""}
+                ${cliente.telefono || ""}
+                ${cliente.direccion || ""}
+            `
+            .toLowerCase()
+            .includes(
+                filtro.toLowerCase()
+            )
+        );
+
+    lista.innerHTML = "";
+
+    clientes.forEach(cliente => {
+
+        lista.innerHTML += `
+            <div
+                class="card-cliente"
+                data-id="${cliente.id}"
+            >
+                <h3>${cliente.nombre}</h3>
+
+                <p>
+                    ${cliente.encargado || ""}
+                </p>
+
+                <p>
+                    ${cliente.telefono || ""}
+                </p>
+            </div>
+        `;
+
+    });
+
+    document
+        .querySelectorAll(".card-cliente")
+        .forEach(card => {
+
+            card.addEventListener(
+                "click",
+                () => abrirCliente(
+                    card.dataset.id
+                )
+            );
+
+        });
+
+}
+
+function abrirCliente(id){
+
+    const cliente =
+        getClienteById(id);
+
+    const detalle =
+        document.getElementById(
+            "detalleCliente"
+        );
+    
+    const productos =
+        getProductosCliente(id);
+    
+    detalle.innerHTML = `
+    
+        <div class="cliente-detalle">
+
+        <h3>Detalle del Cliente</h3>
+        
+            <h2>${cliente.nombre}</h2>
+
+            <p>
+                Encargado:
+                ${cliente.encargado || ""}
+            </p>
+
+            <p>
+                Teléfono:
+                ${cliente.telefono || ""}
+            </p>
+
+            <p>
+                Dirección:
+                ${cliente.direccion || ""}
+            </p>
+
+            <button
+                id="btnAgregarProducto"
+            >                
+                Agregar Producto
+            </button>
+
+            <button id="btnEditarCliente">
+                Editar Cliente
+            </button>
+
+            <button id="btnEliminarCliente">
+                Eliminar Cliente
+            </button>
+
+<div id="productosCliente">
+
+    ${
+        productos.map(producto => `
+        
+            <div
+                class="producto-card"
+                data-producto="${producto.producto}"
+                data-presentacion="${producto.presentacion}"
+            >
+            
+                <strong>
+                    ${producto.producto}
+                </strong>
+
+                <p>
+                    ${producto.presentacion}
+                </p>
+
+                <p>
+                    $${producto.precio}
+                </p>
+
+                   <button
+                    class="btnEditarProducto"
+                    data-id="${producto.id}"
+                >
+                    Editar
+                </button>
+            
+                <button
+                    class="btnEliminarProducto"
+                    data-id="${producto.id}"
+                >
+                    Eliminar
+                </button>
+
+            </div>
+            
+        `).join("")
+    }
+
+</div>
+
+        </div>
+    `;
+
+        document
+        .getElementById(
+            "btnAgregarProducto"
+        )
+        .addEventListener(
+            "click",
+            () => nuevoProducto(id)
+        );
+
+            document
+            .getElementById(
+                "btnEditarCliente"
+            )
+            .addEventListener(
+                "click",
+                () => editarClienteUI(id)
+            );
+        
+        document
+            .getElementById(
+                "btnEliminarCliente"
+            )
+            .addEventListener(
+                "click",
+                () => eliminarClienteUI(id)
+            );
+    
+        document
+            .querySelectorAll(
+                ".btnEditarProducto"
+            )
+            .forEach(btn => {
+        
+                btn.addEventListener(
+                    "click",
+                    () =>
+                        editarProductoUI(
+                            btn.dataset.id,
+                            id
+                        )
+                );
+        
+            });
+        
+        document
+            .querySelectorAll(
+                ".btnEliminarProducto"
+            )
+            .forEach(btn => {
+        
+                btn.addEventListener(
+                    "click",
+                    () =>
+                        eliminarProductoUI(
+                            btn.dataset.id,
+                            id
+                        )
+                );
+        
+            });
+
+            document
+                .querySelectorAll(
+                    ".producto-card"
+                )
+                .forEach(card => {
+            
+                    card.addEventListener(
+                        "click",
+                        () =>
+                            mostrarResumenProducto(
+                                card.dataset.producto,
+                                card.dataset.presentacion
+                            )
+                    );
+            
+                });
+    
+}
+
+function nuevoCliente(){
+
+    const nombre =
+        prompt("Nombre cliente");
+
+    if(!nombre) return;
+
+    const encargado =
+        prompt("Encargado");
+
+    const telefono =
+        prompt("Teléfono");
+
+    const direccion =
+        prompt("Dirección");
+
+    crearCliente({
+
+        nombre,
+
+        encargado,
+
+        telefono,
+
+        direccion
+
+    });
+
+    renderClientes();
+}
+
+function editarClienteUI(id){
+
+    const cliente =
+        getClienteById(id);
+
+    const nombre =
+        prompt(
+            "Nombre",
+            cliente.nombre
+        );
+
+    if(!nombre) return;
+
+    const encargado =
+        prompt(
+            "Encargado",
+            cliente.encargado || ""
+        );
+
+    const telefono =
+        prompt(
+            "Teléfono",
+            cliente.telefono || ""
+        );
+
+    const direccion =
+        prompt(
+            "Dirección",
+            cliente.direccion || ""
+        );
+
+    editarCliente(
+        id,
+        {
+            nombre,
+            encargado,
+            telefono,
+            direccion
+        }
+    );
+
+    renderClientes();
+
+    abrirCliente(id);
+}
+
+function eliminarClienteUI(id){
+
+    const confirmar =
+        confirm(
+            "¿Eliminar cliente?"
+        );
+
+    if(!confirmar) return;
+
+    eliminarCliente(id);
+
+    document
+        .getElementById(
+            "detalleCliente"
+        )
+        .innerHTML = "";
+
+    renderClientes();
+}
+
+export {
+    renderClientes,
+    abrirCliente
+};
