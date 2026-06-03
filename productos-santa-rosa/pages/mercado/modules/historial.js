3b112403b7b650e7eedf00f18fe82d12a339af8a
faramag-net <faratool@gmail.com>
2026-06-03 12:01:04 -0600

    Update historial.js

--- productos-santa-rosa/pages/mercado/modules/historial.js
+++ productos-santa-rosa/pages/mercado/modules/historial.js
@@ -1,1 +1,51 @@
+import LocalDB from "../../../core/storage/local-db.js";
+
+export function registrarMovimiento(
+    movimiento
+){
+
+    const historial =
+        LocalDB.getMercadoHistory() || [];
+
+    historial.push({
+
+        id:
+            crypto.randomUUID(),
+
+        fecha:
+            new Date()
+                .toISOString(),
+
+        ...movimiento
+
+    });
+
+    LocalDB.saveMercadoHistory(
+        historial
+    );
+
+}
+
+export function getMovimientos(){
+
+    return (
+        LocalDB.getMercadoHistory()
+        || []
+    )
+    .sort(
+        (a,b) =>
+            new Date(b.fecha) -
+            new Date(a.fecha)
+    );
+
+}
+
+export function eliminarHistorial(){
+
+    LocalDB.saveMercadoHistory(
+        []
+    );
+
+}
+
 
