10588d0f27bb405db3c1fd681b532143d6ad1680
faramag-net <faratool@gmail.com>
2026-06-03 13:52:36 -0600

    Update local-db.js

--- productos-santa-rosa/core/storage/local-db.js
+++ productos-santa-rosa/core/storage/local-db.js
@@ -11,7 +11,7 @@ const DB_KEYS = {
   VISITS: "psr_visits",
   SETTINGS: "psr_settings",
   HISTORY: "psr_history",
-  MERCADO_HISTORY: "psr_mercado_history",
+  MERCADO_HISTORY: "psr_mercado_history"
 };
 
 class LocalDB {
