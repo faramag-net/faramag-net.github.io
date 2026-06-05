f8cc97680043bb420764858f4ce7296c32bf1822
faramag-net <faratool@gmail.com>
2026-06-05 15:36:14 -0600

    Update insumos.js

--- productos-santa-rosa/pages/insumos/insumos.js
+++ productos-santa-rosa/pages/insumos/insumos.js
@@ -1,1 +1,21 @@
+const btnHistorial =
+    document.getElementById("toggleHistorial");
 
+const historialContainer =
+    document.getElementById("historialContainer");
+
+let abierto = false;
+
+btnHistorial.addEventListener("click", () => {
+
+    abierto = !abierto;
+
+    historialContainer.style.display =
+        abierto ? "block" : "none";
+
+    btnHistorial.textContent =
+        abierto
+            ? "▼ Historial (0)"
+            : "▶ Historial (0)";
+
+});
