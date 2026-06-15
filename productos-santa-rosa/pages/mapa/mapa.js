28a29266e527ff50d85d7b58fca69c0cf9abd303
faramag-net <faratool@gmail.com>
2026-06-14 19:31:06 -0600

    Update mapa.js

--- productos-santa-rosa/pages/mapa/mapa.js
+++ productos-santa-rosa/pages/mapa/mapa.js
@@ -18,6 +18,21 @@ L.tileLayer(
 
 ).addTo(mapa);
 
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+
+    iconRetinaUrl:
+        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+
+    iconUrl:
+        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+
+    shadowUrl:
+        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png"
+
+});
+
 const clientes =
     JSON.parse(
         localStorage.getItem(
