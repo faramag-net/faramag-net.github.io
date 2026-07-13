d5bb8fdcd3b5fe096ffb8c4f9d9c11d5745bda2f
faramag-net <faratool@gmail.com>
2026-07-13 12:19:43 -0600

    primeros pasos

---
+++ PDV/core/constants.js
@@ -0,0 +1,19 @@
+/**
+ * ---------------------------------------------------
+ * PDV
+ * Archivo:
+ * Descripción:
+ * Versión:
+ * Autor:
+ * ---------------------------------------------------
+ */
+
+const APP = {
+
+    NAME:"PDV",
+
+    VERSION:"0.1.0",
+
+    AUTHOR:"Ricardo Farabundo Magallón Juan-Qui"
+
+};
